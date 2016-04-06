fabc49dbf0d357c45d67f0b429a83b630a8babab
Corban Baxter <corbanb@gmail.com>
2016-04-06 13:54:18 -0600

    adding todos

--- src/Mailchimp.js
+++ src/Mailchimp.js
@@ -7,6 +7,7 @@ export default class Mailchimp {
 		if(!dc) throw new Error('No Mailchimp Data Center provided. Please see Mailchimp to get your accounts Data Center.');
 
 		// TODO: add test for promises and throw error if no polyfill is given or provide the polyfill
+		// TODO: what options should we prevent from happening?
 
 		const defaults = {
 			baseURL: 'https://${dc}.api.mailchimp.com/3.0/',
--- src/Mandrill.js
+++ src/Mandrill.js
@@ -6,6 +6,7 @@ export default class Mandrill {
 		if(!apiKey) throw new Error('No Mandrill API key given to monkey-around. Please see Mandrill to get your API Key.');
 
 		// TODO: add test for promises and throw error if no polyfill is given or provide the polyfill
+		// TODO: what options should we prevent from happening?
 
 		const defaults = {
 			baseURL: 'https://${dc}.api.mailchimp.com/3.0/',
