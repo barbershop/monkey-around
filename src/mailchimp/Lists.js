9b4eb4cc3670dc4164eb9ab3d579188aba40a6ea
Corban Baxter <corbanb@gmail.com>
2016-04-06 15:43:00 -0600

    more generic lists

--- src/mailchimp/Lists.js
+++ src/mailchimp/Lists.js
@@ -1,28 +1,35 @@
 import Mailchimp from './Mailchimp';
+import _ from 'lodash';
 
 export class List extends Mailchimp {
+
+	constructor(apiKey, dc, options){
+		super(apiKey, dc, options);
+	}
 
 	// subscribe a user to a specfic list
 	// Member Object - https://api.mailchimp.com/schema/3.0/Lists/Members/Instance.json?_ga=1.252183759.770295981.1457393059
 	subscribe(list, params) {
 
 		return this.request({
-			url: `/3.0/lists/${list}/members/`,
+			url: `/lists/${list}/members/`,
 			method: 'post',
 			data: params
 		});
+
 	}
 
 	updateSubscriber(list, params) {
 
+		// create md5 hash of the email address to the the member id for mailchimp
 		if(!params.email_address) throw new Error('No email address was given in the params');
-
 		const MEMBER_HASH = this.memberHash(params.email_address);
 
 		return this.request({
-			url: `/3.0/lists/${list}/members/${MEMBER_HASH}`,
+			url: `/lists/${list}/members/${MEMBER_HASH}`,
 			method: 'put',
 			data: params
 		});
+
 	}
 }
